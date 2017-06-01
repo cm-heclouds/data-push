56503578c7113ec75eec8a9d0d5bbf1922645475
skyinone <skyinone@163.com>
2017-06-01 09:26:41 +0800

    modified:   JAVA/src/main/java/com/onenet/datapush/receiver/ReceiverDemo.java

--- JAVA/src/main/java/com/onenet/datapush/receiver/ReceiverDemo.java
+++ JAVA/src/main/java/com/onenet/datapush/receiver/ReceiverDemo.java
@@ -31,8 +31,9 @@ public class ReceiverDemo {
     /**
      * 功能描述：第三方平台数据接收。<p>
      *           <ul>注:
-     *               <li>1.OneNet平台为了保证数据不丢失，有重发机制，如果重复数据对业务有意向，数据接收端需要对重复数据进行排除重复处理。</li>
-     *               <li>2.OneNet每一次post数据请求设有时限，接收程序接收到数据时，尽量先缓存起来，再做业务逻辑处理。</li>
+     *               <li>1.OneNet平台为了保证数据不丢失，有重发机制，如果重复数据对业务有影响，数据接收端需要对重复数据进行排除重复处理。</li>
+     *               <li>2.OneNet每一次post数据请求后，等待客户端的响应都设有时限，在规定时限内没有收到响应会认为发送失败。
+     *                    接收程序接收到数据时，尽量先缓存起来，再做业务逻辑处理。</li>
      *           </ul>
      * @param body 数据消息
      * @return 任意字符串。OneNet平台接收到http 200的响应，才会认为数据推送成功，否则会重发。
@@ -40,12 +41,15 @@ public class ReceiverDemo {
     @RequestMapping(value = "/receive",method = RequestMethod.POST)
     @ResponseBody
     public String receive(@RequestBody String body) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        /**
-         *  解析数据推送请求，非加密模式
-         */
 
+        logger.info("data receive:  body String --- " +body);
+        /************************************************
+         *  解析数据推送请求，非加密模式。
+         *  如果是明文模式使用以下代码
+         **************************************************/
+        /*************明文模式  start****************/
         Util.BodyObj obj = Util.resolveBody(body, false);
-        logger.info("data receive:  body --- " +obj);
+        logger.info("data receive:  body Object --- " +obj);
         if (obj != null){
             boolean dataRight = Util.checkSignature(obj, token);
             if (dataRight){
@@ -57,12 +61,17 @@ public class ReceiverDemo {
         }else {
             logger.info("data receive: body empty error");
         }
+        /*************明文模式  end****************/
 
-        /**
+
+        /********************************************************
          *  解析数据推送请求，加密模式
-         */
+         *
+         *  如果是加密模式使用以下代码
+         ********************************************************/
+        /*************加密模式  start****************/
 //        Util.BodyObj obj1 = Util.resolveBody(body, true);
-//        logger.info("data receive:  body --- " +obj1);
+//        logger.info("data receive:  body Object--- " +obj1);
 //        if (obj1 != null){
 //            boolean dataRight1 = Util.checkSignature(obj1, token);
 //            if (dataRight1){
@@ -74,6 +83,7 @@ public class ReceiverDemo {
 //        }else {
 //            logger.info("data receive: body empty error" );
 //        }
+        /*************加密模式  end****************/
          return "ok";
     }
 
